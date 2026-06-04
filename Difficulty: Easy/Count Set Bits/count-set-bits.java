dfe2eef1e4c786d8007ae066a23b716412a483c7
Prakhar Yadav <137612937+yadavxprakhar@users.noreply.github.com>
2026-06-04 17:05:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Set Bits/count-set-bits.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+class Solution {
+    static int setBits(int n) {
+        int cnt = 0;
+        while(n > 0) {
+            n = n & (n - 1);
+            cnt++;
+        }
+        return cnt;
+    }
+}
